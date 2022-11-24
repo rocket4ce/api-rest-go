28d7599a417d86b819d6ac20af8508e7a99fe298
Dinko <rocket4ce@gmail.com>
2022-11-23 22:37:52 -0300

    add: routes

---
+++ routes.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name       string
+	Method     string
+	Pattern    string
+	HandleFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Name(route.Name).
+			Methods(route.Method).
+			Path(route.Pattern).
+			Handler(route.HandleFunc)
+
+	}
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"MovieList",
+		"GET",
+		"/movies",
+		MovieList,
+	},
+	Route{
+		"GetMovie",
+		"GET",
+		"/movies/{id}",
+		GetMovie,
+	},
+}
